ed071f459c9560fe1235b7a47e1b8216ed4207c3
Semko1010 <semir01020@gmail.com>
2021-12-20 16:47:21 +0100

    test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Header and Footer
 import Header from "./components/Header/Header"
-<<<<<<< HEAD
-import SubCaterogy from "./components/SubCategory/SubCategory"
-import SubCategori from './components/SubCategory/SubCategory'
-=======
 import Footer from "./components/Footer/Footer"
 
 
@@ -15,18 +11,10 @@ import Category from './components/Category/Category';
 import SubCategory from './components/SubCategory/SubCategory';
 import Detail from './components/Detail/Detail';
 import CategoryTemplate from "./components/Category/CategoryTemplate"
->>>>>>> 0fed0cf326bebc44e09e2ddcf40c44e10fb81bea
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-     
-     
-      <Header />
-      <SubCaterogy/>
-      
-=======
       <Router>
         <Header />
         <Routes>
@@ -36,7 +24,6 @@ function App() {
         </Routes>
       </Router>
       <Footer />
->>>>>>> 0fed0cf326bebc44e09e2ddcf40c44e10fb81bea
     </div>
   );
 }
