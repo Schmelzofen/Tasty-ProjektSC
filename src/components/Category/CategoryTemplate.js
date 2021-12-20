454e59d2b6627e996e88ab13affae069dcbca26a
Sertac Özdemir <sertac.oezdemir@gmail.com>
2021-12-20 16:28:52 +0100

    vorläufig

--- src/components/Category/CategoryTemplate.js
+++ src/components/Category/CategoryTemplate.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const CategoryTemplate = (props) => {
     return (
         <article className="CategoryItem">
-            <Link className="link" to="/">
+            <Link className="link" to={`/category/${props.name}`}>
                 <p>{props.name}</p>
                 <img src={props.picture} alt={props.name} />
             </Link>
