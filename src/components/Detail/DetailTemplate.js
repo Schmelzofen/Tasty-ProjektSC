9213c905b85d6840f15cd5a52c57fc121ee4d669
Sertac Özdemir <sertac.oezdemir@gmail.com>
2021-12-20 23:13:27 +0100

    Styles usw

--- src/components/Detail/DetailTemplate.js
+++ src/components/Detail/DetailTemplate.js
@@ -5,77 +5,79 @@ const DetailTemplate = ({ name, picture, instructions, zutat1, zutat2, zutat3, z
     return (
         <section className="DetailItem">
             <img src={picture} alt={name} />
-            <article className="DetailLeft">
-                <h2>{name}</h2>
-                <p>{instructions}</p>
-            </article>
-            <article className="DetailRight">
-                <h2>Ingredients</h2>
-                <ul className="Ingredients">
-                    <div>
-                        <span>{mass1}</span>
-                        <span>{zutat1}</span>
-                    </div>
-                    <div>
-                        <span>{mass2}</span>
-                        <span>{zutat2}</span>
-                    </div>
-                    <div>
-                        <span>{mass3}</span>
-                        <span>{zutat3}</span>
-                    </div>
-                    <div>
-                        <span>{mass4}</span>
-                        <span>{zutat4}</span>
-                    </div>
-                    <div>
-                        <span>{mass5}</span>
-                        <span>{zutat5}</span>
-                    </div>
-                    <div>
-                        <span>{mass6}</span>
-                        <span>{zutat6}</span>
-                    </div>
-                    <div>
-                        <span>{mass7}</span>
-                        <span>{zutat7}</span>
-                    </div>
-                    <div>
-                        <span>{mass8}</span>
-                        <span>{zutat8}</span>
-                    </div>
-                    <div>
-                        <span>{mass9}</span>
-                        <span>{zutat9}</span>
-                    </div>
-                    <div>
-                        <span>{mass10}</span>
-                        <span>{zutat10}</span>
-                    </div>
-                    <div>
-                        <span>{mass11}</span>
-                        <span>{zutat11}</span>
-                    </div>
-                    <div>
-                        <span>{mass12}</span>
-                        <span>{zutat12}</span>
-                    </div>
-                    <div>
-                        <span>{mass13}</span>
-                        <span>{zutat13}</span>
-                    </div>
-                    <div>
-                        <span>{mass14}</span>
-                        <span>{zutat14}</span>
-                    </div>
-                    <div>
-                        <span>{mass15}</span>
-                        <span>{zutat15}</span>
-                    </div>
-                </ul>
-                <button className="button"><a href="">Watch on YouTube</a></button>
-            </article>
-        </section>
+            <section className="DetailFlex">
+                <article className="DetailLeft">
+                    <h2>{name}</h2>
+                    <p>{instructions}</p>
+                </article>
+                <article className="DetailRight">
+                    <h2>Ingredients</h2>
+                    <ul className="Ingredients">
+                        <div>
+                            <span>{mass1}</span>
+                            <span>{zutat1}</span>
+                        </div>
+                        <div>
+                            <span>{mass2}</span>
+                            <span>{zutat2}</span>
+                        </div>
+                        <div>
+                            <span>{mass3}</span>
+                            <span>{zutat3}</span>
+                        </div>
+                        <div>
+                            <span>{mass4}</span>
+                            <span>{zutat4}</span>
+                        </div>
+                        <div>
+                            <span>{mass5}</span>
+                            <span>{zutat5}</span>
+                        </div>
+                        <div>
+                            <span>{mass6}</span>
+                            <span>{zutat6}</span>
+                        </div>
+                        <div>
+                            <span>{mass7}</span>
+                            <span>{zutat7}</span>
+                        </div>
+                        <div>
+                            <span>{mass8}</span>
+                            <span>{zutat8}</span>
+                        </div>
+                        <div>
+                            <span>{mass9}</span>
+                            <span>{zutat9}</span>
+                        </div>
+                        <div>
+                            <span>{mass10}</span>
+                            <span>{zutat10}</span>
+                        </div>
+                        <div>
+                            <span>{mass11}</span>
+                            <span>{zutat11}</span>
+                        </div>
+                        <div>
+                            <span>{mass12}</span>
+                            <span>{zutat12}</span>
+                        </div>
+                        <div>
+                            <span>{mass13}</span>
+                            <span>{zutat13}</span>
+                        </div>
+                        <div>
+                            <span>{mass14}</span>
+                            <span>{zutat14}</span>
+                        </div>
+                        <div>
+                            <span>{mass15}</span>
+                            <span>{zutat15}</span>
+                        </div>
+                    </ul>
+                    <button className="button"><a href="">Watch on YouTube</a></button>
+                </article>
+            </section>
+        </section >
     );
 }
 
