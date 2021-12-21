d08344eecea723733063578b1e011d83c8ad2a8b
Semko1010 <semir01020@gmail.com>
2021-12-21 14:46:54 +0100

    test

--- src/components/Detail/DetailTemplate.js
+++ src/components/Detail/DetailTemplate.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-import { useParams } from "react-router-dom";
-import { Component } from "react"
-// Details URL :
-// last element is ID
-// https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772
-
-
-
-
-class DetailTemplate extends Component {
-    state = {
-        DetailData: []
-    }
-    componentDidMount() {
-        let { id } = useParams()
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
-            .then(response => response.json())
-            .then(json => this.setState({ DetailData: json }))
-    }
-
-    render() {
-
-        let data = this.state.DetailData.filter(i => i.data === id)
-        return (
-            <section className="DetailItem">
-                <img src={data.strMealThumb} alt="DetailDescription" />
-                <article className="DetailLeft">
-                    <h2>{data.strMeal}</h2>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Temporibus consequuntur similique tempora ex a, quod adipisci autem dolor excepturi et enim. Impedit nisi odio aperiam architecto cumque aspernatur, velit voluptas accusantium, dolore repellendus recusandae earum quod reprehenderit cupiditate libero eius assumenda consequuntur! Facere doloremque autem magni mollitia ut commodi dolorum omnis rem voluptates perferendis exercitationem asperiores accusantium consectetur, maiores quia veniam architecto nisi! Temporibus quae voluptatibus beatae natus, est quo aut vero, odio nobis doloribus molestiae, alias voluptas molestias magni tenetur sed impedit dolore quis ipsum perferendis fugit culpa. Ratione soluta maxime dolor, aut iure voluptatem reiciendis nulla dicta cupiditate.</p>
-                </article>
-                <article className="DetailRight">
-                    <h2>Ingredients</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita officiis nobis perferendis facere velit cupiditate dicta? Doloremque, saepe labore! Unde sunt dignissimos ab voluptas, ex at asperiores? Saepe tenetur, ab earum tempore eligendi magnam qui amet repudiandae laboriosam culpa similique maiores nam itaque quo labore,</p>
-                    <a href={data.strYoutube} >Watch on YouTube</a>
-=======
 const DetailTemplate = ({ youtube, name, picture, instructions, zutat1, zutat2, zutat3, zutat4, zutat5, zutat6, zutat7, zutat8, zutat9, zutat10, zutat11, zutat12, zutat13, zutat14, zutat15, mass1, mass2, mass3, mass4, mass5, mass6, mass7, mass8, mass9, mass10, mass11, mass12, mass13, mass14, mass15 }) => {
     return (
         <section className="DetailItem">
@@ -108,7 +72,6 @@ const DetailTemplate = ({ youtube, name, picture, instructions, zutat1, zutat2, 
                         </div>
                     </ul>
                     <button className="button"><a href={youtube}>Watch on YouTube</a></button>
->>>>>>> main
                 </article>
             </section>
         </section >
--- src/components/SubCategory/SubCategoryTemplate.js
+++ src/components/SubCategory/SubCategoryTemplate.js
@@ -1,44 +1,6 @@
 import { Link } from "react-router-dom"
 
 const SubCategoryTemplate = (props) => {
-<<<<<<< HEAD
-   
-    
-        return (
-            <article className="SubCategoryItem">
-                <p>{props.title}</p>
-                <Link to={`/SubCategory/${props.title}`}>Details</Link>
-                <img src={props.src} alt="SubCatDescription" />
-            </article>
-        );
-    
-
-   
-
-
-} 
-export default SubCategoryTemplate;
-
-
-// import { Link } from 'react-router-dom'
-// const SubCategoryTemplate = (props) => {
-   
-    
-//         return (
-//             <article className="SubCategoryItem">
-//                 <p>SubCategory Item</p>
-//                 <Link to={`/details/${props.id}`}>Details</Link>
-//                 <img src={props.img} alt="SubCatDescription" />
-//             </article>
-//         );
-    
-
-   
-
-
-// } 
-// export default SubCategoryTemplate;
-=======
     return (
         <article className="SubCategoryItem">
             <Link className="link" to={`/details/${props.id}`}>
@@ -50,4 +12,3 @@ export default SubCategoryTemplate;
 }
 
 export default SubCategoryTemplate;
->>>>>>> main
