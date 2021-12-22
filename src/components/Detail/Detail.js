9583c5763c0addb5c84fcc925f48a5677ed6af8d
Sertac Özdemir <sertac.oezdemir@gmail.com>
2021-12-22 13:57:28 +0100

    22.12. 13:57

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,8 +1,10 @@
 import DetailTemplate from "./DetailTemplate"
 import React, { Component } from 'react';
-import { Link } from "react-router-dom"
 import { v4 as uuidv4 } from 'uuid';
-let test2 = [];
+let strIngredient = [];
+let strMeasure = [];
+let strDesc = [];
+
 class Detail extends React.Component {
     constructor(props) {
         super(props);
@@ -12,48 +14,45 @@ class Detail extends React.Component {
 
         }
     }
-
     componentDidMount() {
         fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${this.props.id}`)
             .then(response => response.json())
             .then(json => this.setState({ subData: json.meals }))
     }
-
-    // fetchIngredients = () => {
-    //     console.log(this.state.subData)
-    //     this.state.subData.forEach(e => {
-    //         for (let i = 0; i < 20; i++) {
-    //             console.log(e.strIngredient1);
-    //         }
-    //     })
-    // }
     fetchIngredients2 = () => {
-        console.log(this.state.subData)
         this.state.subData.forEach(e => {
+            let text = this.state.subData[0].strInstructions
+            strDesc = text.split('.')
+            strDesc = strDesc.map(i => i + '.')
+            strDesc.pop()
+            console.log(strDesc)
+        });
+
+        this.state.subData.forEach(e => {
+            strIngredient = []
             for (let i = 0; i < 20; i++) {
                 let n = `strIngredient${i}`
                 let ne = `e.${n}`
                 if (eval(ne)) {
-                    // console.log(eval(ne));
-                    test2.push(eval(ne))
+                    strIngredient.push(eval(ne))
                 }
             }
-        })
-        console.log(test2);
+        });
+        this.state.subData.forEach(e => {
+            strMeasure = []
+            for (let i = 0; i < 20; i++) {
+                let n = `strMeasure${i}`
+                let ne = `e.${n}`
+                if (eval(ne)) {
+                    strMeasure.push(eval(ne))
+                }
+            }
+        });
+
 
     }
-
-
-
-
-
-
     render() {
-
-        // this.fetchIngredients()
         this.fetchIngredients2()
-
-        console.log(this.state.test)
         return (
             <div>
                 {this.state.subData.map(i => <DetailTemplate
@@ -61,42 +60,13 @@ class Detail extends React.Component {
                     name={i.strMeal}
                     picture={i.strMealThumb}
                     instructions={i.strInstructions}
-                    mass1={i.strMeasure1}
-                    mass2={i.strMeasure2}
-                    mass3={i.strMeasure3}
-                    mass4={i.strMeasure4}
-                    mass5={i.strMeasure5}
-                    mass6={i.strMeasure6}
-                    mass7={i.strMeasure7}
-                    mass8={i.strMeasure8}
-                    mass9={i.strMeasure9}
-                    mass10={i.strMeasure10}
-                    mass11={i.strMeasure11}
-                    mass12={i.strMeasure12}
-                    mass13={i.strMeasure13}
-                    mass14={i.strMeasure14}
-                    mass15={i.strMeasure15}
-
-                    zutat1={i.strIngredient1}
-                    zutat2={i.strIngredient2}
-                    zutat3={i.strIngredient3}
-                    zutat4={i.strIngredient4}
-                    zutat5={i.strIngredient5}
-                    zutat6={i.strIngredient6}
-                    zutat7={i.strIngredient7}
-                    zutat8={i.strIngredient8}
-                    zutat9={i.strIngredient9}
-                    zutat10={i.strIngredient10}
-                    zutat11={i.strIngredient11}
-                    zutat12={i.strIngredient12}
-                    zutat13={i.strIngredient13}
-                    zutat14={i.strIngredient14}
-                    zutat15={i.strIngredient15}
                     youtube={i.strYoutube}
                 />)}
+
             </div>
         )
     }
 }
 
+export { strIngredient, strMeasure, strDesc };
 export default Detail;
--- src/components/Detail/DetailTemplate.js
+++ src/components/Detail/DetailTemplate.js
@@ -1,74 +1,42 @@
-const DetailTemplate = ({ youtube, name, picture, instructions, zutat1, zutat2, zutat3, zutat4, zutat5, zutat6, zutat7, zutat8, zutat9, zutat10, zutat11, zutat12, zutat13, zutat14, zutat15, mass1, mass2, mass3, mass4, mass5, mass6, mass7, mass8, mass9, mass10, mass11, mass12, mass13, mass14, mass15 }) => {
+import { strIngredient } from "./Detail";
+import { strMeasure } from "./Detail";
+import { strDesc } from "./Detail";
+import { v4 as uuidv4 } from 'uuid';
+const DetailTemplate = ({ youtube, name, picture, instructions, zutat, zutat1, zutat2, zutat3, zutat4, zutat5, zutat6, zutat7, zutat8, zutat9, zutat10, zutat11, zutat12, zutat13, zutat14, zutat15, mass1, mass2, mass3, mass4, mass5, mass6, mass7, mass8, mass9, mass10, mass11, mass12, mass13, mass14, mass15 }) => {
+
     return (
-        <section className="DetailItem">
+
+        < section className="DetailItem" >
             <img src={picture} alt={name} />
             <section className="DetailFlex">
                 <article className="DetailLeft">
-                    <h2>{name}</h2>
-                    <p>{instructions}</p>
+
+                    <ul>
+                        {strDesc.map(e =>
+
+                            <li>{e}</li>
+
+                        )}
+                    </ul>
                 </article>
                 <article className="DetailRight">
                     <h2>Ingredients</h2>
                     <ul className="Ingredients">
-                        <div>
-                            <span>{mass1}</span>
-                            <span>{zutat1}</span>
+
+                        <div className="strIngredient">
+                            {strIngredient.map(e =>
+                                <li>{e}</li>
+
+                            )}
                         </div>
-                        <div>
-                            <span>{mass2}</span>
-                            <span>{zutat2}</span>
-                        </div>
-                        <div>
-                            <span>{mass3}</span>
-                            <span>{zutat3}</span>
-                        </div>
-                        <div>
-                            <span>{mass4}</span>
-                            <span>{zutat4}</span>
-                        </div>
-                        <div>
-                            <span>{mass5}</span>
-                            <span>{zutat5}</span>
-                        </div>
-                        <div>
-                            <span>{mass6}</span>
-                            <span>{zutat6}</span>
-                        </div>
-                        <div>
-                            <span>{mass7}</span>
-                            <span>{zutat7}</span>
-                        </div>
-                        <div>
-                            <span>{mass8}</span>
-                            <span>{zutat8}</span>
-                        </div>
-                        <div>
-                            <span>{mass9}</span>
-                            <span>{zutat9}</span>
-                        </div>
-                        <div>
-                            <span>{mass10}</span>
-                            <span>{zutat10}</span>
-                        </div>
-                        <div>
-                            <span>{mass11}</span>
-                            <span>{zutat11}</span>
-                        </div>
-                        <div>
-                            <span>{mass12}</span>
-                            <span>{zutat12}</span>
-                        </div>
-                        <div>
-                            <span>{mass13}</span>
-                            <span>{zutat13}</span>
-                        </div>
-                        <div>
-                            <span>{mass14}</span>
-                            <span>{zutat14}</span>
-                        </div>
-                        <div>
-                            <span>{mass15}</span>
-                            <span>{zutat15}</span>
+                    </ul>
+
+                    <ul className="Measure">
+                        <div className="strMeasure">
+                            {strMeasure.map(e =>
+                                <p>{e}</p>
+
+                            )}
                         </div>
                     </ul>
                     <button className="button"><a href={youtube}>Watch on YouTube</a></button>
